0c1b15bf6d5abe4af8a616a77a6ff2f955abaf62
Morgan <s.morgan.jeffries@gmail.com>
2014-07-03 00:04:04 -0400

    Fixes a problem where unit tests for parseResourceActions weren't being run.

--- test/unit/spec/resource/parseResourceActionsSpec.js
+++ test/unit/spec/resource/parseResourceActionsSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-xdescribe('Factory: parseResourceActions', function () {
+describe('Factory: parseResourceActions', function () {
   var parseResourceActions,
     mainAction,
     action,
